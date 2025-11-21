46fa9b4f53de0943e306d55fc6b557d5aa7a5129
jongithub59 <jdelrealj01@gmail.com>
2025-11-21 15:56:28 -0600

    move Card loginc to GameContainer

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,9 @@ import Header from "./components/Header";
 import DifficultyContainer from "./components/DifficultyContainer";
 import ScoreTracker from "./components/ScoreTracker";
 import GameContainer from "./components/GameContainer";
-import Card from "./components/Card";
-import shiv from "./assets/shiv.png";
-import pocket from "./assets/pocket.png";
-import holliday from "./assets/holliday.png";
-import warden from "./assets/warden.png";
 import "./styles/App.css";
 
 function App() {
-  // array of card objects containing their name and image url, can change later to fetch from api later
-  const heroes = [
-    {
-      name: "Shiv",
-      img: shiv,
-    },
-    {
-      name: "Pocket",
-      img: pocket,
-    },
-    {
-      name: "Holliday",
-      img: holliday,
-    },
-    {
-      name: "Warden",
-      img: warden,
-    },
-  ];
-
-  // create an array of Card components from hero array to then render with GameContainer
-  const heroCards = heroes.map((hero) => {
-    return <Card key={hero.name} url={hero.img} name={hero.name} />; // props for each Card
-  });
-
   return (
     <>
       <div className="app">
@@ -43,7 +13,7 @@ function App() {
           <ScoreTracker></ScoreTracker>
           <DifficultyContainer></DifficultyContainer>
         </Header>
-        <GameContainer>{heroCards}</GameContainer>
+        <GameContainer></GameContainer>
       </div>
     </>
   );
--- src/components/GameContainer.jsx
+++ src/components/GameContainer.jsx
@@ -1,5 +1,45 @@
-function GameContainer({ children }) {
-  return <div className="game-container">{children}</div>;
+import Card from "./Card";
+import shiv from "../assets/shiv.png";
+import pocket from "../assets/pocket.png";
+import holliday from "../assets/holliday.png";
+import warden from "../assets/warden.png";
+
+function GameContainer(props) {
+  // array of card objects containing their name and image url, can change later to fetch from api later
+  const heroes = [
+    {
+      name: "Shiv",
+      img: shiv,
+    },
+    {
+      name: "Pocket",
+      img: pocket,
+    },
+    {
+      name: "Holliday",
+      img: holliday,
+    },
+    {
+      name: "Warden",
+      img: warden,
+    },
+  ];
+
+  // create an array of Card components from hero array to then render with GameContainer
+  const heroCards = heroes.map((hero) => {
+    return (
+      <Card
+        // props for each Card
+        key={hero.name}
+        url={hero.img}
+        setGameOver={props.setGameOver}
+        incrementScore={props.incrementScore}
+        name={hero.name}
+      />
+    );
+  });
+
+  return <div className="game-container">{heroCards}</div>;
 }
 
 export default GameContainer;
